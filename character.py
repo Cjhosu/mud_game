e61ccbc15d76e992c021cd6b4f526f1f94b475bf
Caleb Howard <cjhosu@gmail.com>
2020-09-08 21:44:42 -0400

    Add first classes

---
+++ character.py
@@ -0,0 +1,8 @@
+class Character:
+    def __init__(self,name):
+        self.char_name = name
+        self.health = 25
+
+class Level(Character):
+    def __init__(self)
+© 2020 GitHub, Inc.
---
+++ startgame.py
@@ -0,0 +1,7 @@
+from character import Character
+
+print ("oh well hello, what is your name?")
+name = input("")
+Char = Character(name)
+name=Char.char_name
+print("nice to meet you "+ name)
