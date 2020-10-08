6eb41120acba302695ea0979e080f37c58af1307
Caleb Howard <cjhosu@gmail.com>
2020-10-07 22:01:25 -0400

    Combat overhaul v3

--- mygame/world/combat/combat.py
+++ mygame/world/combat/combat.py
@@ -3,6 +3,7 @@ import random
 
 class CombatHandler():
     target = {}
+    defense_score ={}
     caller = {}
     charclass_attack_attr_dict = {
         "Ranger" : "dex",
@@ -24,28 +25,34 @@ class CombatHandler():
 
     def init_combat(self, caller, target):
         is_equipped = equipped_check(self.caller, "weapon")
+        attack_weapon = self.get_attack_weapon(caller)
+        attack_attr =  self.get_attack_attribute()
+        init_attack_score = self.get_attack_score(attack_weapon,attack_attr)
+        attack_score = round(random.uniform(1.0,1.5)* init_attack_score)
         if target:
             if not caller.search(target, location = caller.location):
                 return
+            else:
+                target = self.caller.search(self.target)
+                defense_score = self.get_defense_score(target)
+                caller.msg(defense_score)
+
+                #What attribute do you use to attack?
+                resolve = self.resolve_attack(target, defense_score, attack_score, attack_weapon)
+                dealt_damage = resolve[0]
+                damage_msg = resolve[1]
+
         else:
-            caller.msg("You test your might...")
+            caller.msg("You test your might... You attack the air with " + str(attack_weapon) +" for an attack score of " + str(attack_score))
+
+    def get_attack_weapon(self, caller):
+        is_equipped = equipped_check(self.caller, "weapon")
         if is_equipped[0] == True:
             slots = caller.db.slots
             attack_weapon = slots["weapon"]
-            #What attribute do you use to attack?
-            attack_attr =  self.get_attack_attribute()
-            init_attack_score = self.get_attack_score(attack_weapon,attack_attr)
-            attack_score = round(random.uniform(1.0,1.5)* init_attack_score)
-
-            #Otherwise use your fists
         else:
-            attack_weapon = "your fists"
-            attack_score = round(random.uniform(1.0,1.5)* caller.db.strength)
-
-        #handle messaging
-        message = "%s +attack%s with %s for a combat score of %s!"
-        caller.msg(message % ("You", "",attack_weapon, attack_score))
-        caller.location.msg_contents(message % (caller.key, "s", attack_weapon, attack_score), exclude=caller)
+            attack_weapon = "your fists" 
+        return attack_weapon
 
     def get_attack_attribute(self):
         caller = self.caller
@@ -68,9 +75,23 @@ class CombatHandler():
         caller = self.caller
         attr_val = caller.attributes.get(attack_attr)
         #Knowing your attack attribute and the weapon equipped, find if it has a buff
-        multiplier = self.weapon_multiplier(weapon, attack_attr)
-        attack_score = attr_val + multiplier
+        if weapon == "your fists":
+            attack_score = caller.db.strength
+        else:
+            multiplier = self.weapon_multiplier(weapon, attack_attr)
+            attack_score = attr_val + multiplier
         return attack_score
 
-class resolve_attack():
-    pass
+    def get_defense_score(self, target):
+        defense_score = target.db.defense
+        if not defense_score:
+            defense_score = 0
+        return defense_score
+
+    def resolve_attack(self, target, defense_score, attack_score, attack_weapon):
+        dealt_damage = attack_score - defense_score
+        if dealt_damage > 0:
+            message = "You've attacked "+ self.target + " for " + str(dealt_damage) + " with "+str(attack_weapon)
+        else:
+            message = self.target + " shrugs off your attack."
+        return dealt_damage, message
