18b4271a3598ac2de8a668068db322ee96248759
Caleb Howard <cjhosu@gmail.com>
2020-10-10 22:54:06 -0400

    Update dialogue for workshop

--- mygame/world/dialogues/caroline.py
+++ mygame/world/dialogues/caroline.py
@@ -1,20 +1,43 @@
 
 def menu_start_node(caller):
-    text = "'Hey there you're looking pretty green!  First time in the magical land of MagicLand?'"
+    text = "'Hey there! How can I help you today?'"
 
     options = (
-            {"desc": "No, we spoke just the other day", "goto" : "END"},
-            {"desc": "Is it that obvious?", "goto": "info1"}
+            {"desc": "Hi there I am new and would like some information!", "goto" : "info1"},
+            {"desc": "Nevermind", "goto" : "END"},
             )
 
     return text, options
 
 def info1(caller):
-    text = "'It is. But hey, welcome to our pub!  Things can get rowdy here at the end of civilization!  Here in Magicland you can be whatever you want as long as it is one of six classes.  When we are done talking just announce it to the room. (say something like 'become Warrior')'"
+    text = "'Welcome to our little nook of sunshine!  Most folks come around here to visit Daryl's shop or declare their professions.  Can I tell you about either those things'"
+
+    options = (
+            {"desc" : "I'd like to learn about professions.", "goto" : "info2"},
+            {"desc" : "Tell me more about Daryl's shop.", "goto" : "info3"},
+            {"desc" : "I think I am good for now.", "goto" : "END"}
+            )
+
+    return text, options
+
+def info2(caller):
+    text = "'It is good that you've come to such a land of opportunity where you can be whatever you want ... as long as it is one of six classes.  When we are done talking just announce it to the room. (say something like 'become Warrior')'"
 
     options = (
             {"desc" : "Sounds great, I am ready to choose!", "goto" : "END"},
+            {"desc" : "I'd like to hear about Daryl's shop", "goto" : "info3"},
             {"desc" : "Let me think about it some more.", "goto" : "END"}
+            )
+
+    return text, options
+
+def info3(caller):
+    text = "'If you head out the exit on the west side of the building and down the garden path you'll see Daryl's workshop.  He is awlays tinkering with something, and he usually has some things to sell.'"
+
+    options = (
+            {"desc" : "Great I'll visit Daryl now.", "goto" : "END"},
+            {"desc" : "I'd like to learn about professions.", "goto" : "info2"},
+            {"desc" : "Those are all the questions I have for now.", "goto" : "END"}
             )
 
     return text, options
